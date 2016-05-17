83b3e0f0844bf7367e37670e94acbf3590da57be
Matt McLaughlin <mclaughlin.matt@gmail.com>
2016-05-17 14:40:37 -0700

    clean up webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,44 +3,44 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    context: __dirname + '/src',
-    entry: './script/main.jsx',
-    resolve: {
-      extensions: ['', '.js', '.jsx', '.scss']
-    },
-    output: {
-      path: __dirname + '/dist',
-      filename: 'bundle.js'
-    },
-		plugins: [
-			new webpack.DefinePlugin({
-				'process.env': {
-					'NODE_ENV': JSON.stringify('production')
-				}
-			}),
-      new ExtractTextPlugin('app.css', {
-            allChunks: true
-      })
-		],
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel',
-          // .babelrc in use
-        },
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
-        }
-      ]
-    },
-    sassLoader: {
-      includePaths: [path.resolve(__dirname, './node_modules')]
-    },
-    postcss: function () {
-      return [
-        require('autoprefixer')({ browsers: ['last 2 versions'] })
-      ];
-    }
+  context: path.join(__dirname, 'src'),
+  entry: './script/main.jsx',
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new ExtractTextPlugin('app.css', {
+      allChunks: true,
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        // .babelrc in use
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass'),
+      },
+    ],
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './node_modules')],
+  },
+  postcss: function postcss() {
+    return [
+      require('autoprefixer')({ browsers: ['last 2 versions'] }),
+    ]
+  },
 }
