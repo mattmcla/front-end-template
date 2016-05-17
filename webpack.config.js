8a35d139e7de9a6ab42bc58a0bb9b139450a7884
Matt McLaughlin <mclaughlin.matt@gmail.com>
2016-05-17 14:34:10 -0700

    Clean up webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
 const path = require('path')
 
 module.exports = {
-    context: __dirname + '/src',
-    entry: './script/main.jsx',
-    resolve: {
-      extensions: ['', '.js', '.jsx', '.scss']
-    },
-    output: {
-      path: __dirname + '/dist',
-      filename: 'bundle.js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel',
-          // .babelrc in use
-        },
-        {
-          test: /\.scss$/,
-          loaders: [
-            'style?sourceMap',
-            'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-            'postcss',
-            //'resolve-url',
-            'sass?sourceMap&sourceMapContents'
-          ]
-        }
-      ]
-    },
-    sassLoader: {
-      includePaths: [path.resolve(__dirname, './node_modules')]
-    },
-    postcss: function () {
-      return [
-        require('autoprefixer')({ browsers: ['last 2 versions'] })
-      ];
-    }
+  context: path.join(__dirname, 'src'),
+  entry: './script/main.jsx',
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        // .babelrc in use
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'postcss',
+          'sass?sourceMap&sourceMapContents',
+        ],
+      },
+    ],
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './node_modules')],
+  },
+  postcss: function postcss() {
+    return [
+      require('autoprefixer')({ browsers: ['last 2 versions'] }),
+    ]
+  },
 }
